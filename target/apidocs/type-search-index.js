b808666f4a6471e17c91e2837aeab0ff6af0bc25
asurazur <asurazur@gmail.com>
2023-10-05 12:53:30 +0800

    Add jar and exe file

--- target/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.mycompany.numericals_completion","l":"Numericals_Completion"},{"p":"com.mycompany.numericals_completion","l":"RegulaFalsi"}];updateSearchResults();
